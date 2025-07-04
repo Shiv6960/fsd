c51a282157099f1a543cfa30c0112ff9d24d5f04
Shiv6960 <Shivvv6969@gmail.com>
2025-07-04 12:38:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function Submit(){
+
+var fname=document.getElementById('fname')
+var lname=document.getElementById('lname')
+var fullname=fname.value+"  "+lname.value
+console.log(fullname)
+var email=document.getElementById('email')
+var Address=document.getElementById('Address')
+var ed=email.value+"  "+Address.value
+console.log(ed)
+}
